e208acc49ec0b40b7fbb76cc25336c47c9163844
Akhror Mirfayizev <mi7akhror@gmail.com>
2023-01-15 23:29:45 -0800

    update

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -8,10 +8,11 @@ display: flex;
 border: 2px solid black;
 width: 80%;
 height: 50vh;
-align-items: center;
-justify-content: space-between;
-margin: 0 10%;
 
+
+margin: 0 10%;
+background-color: coral;
+position: relative;
 
 `
 const Arrow = styled.div`
@@ -20,16 +21,55 @@ height:50px;
 display: flex;
 align-items: center;
 justify-content: center;
+left: ${props=> props.direction === "left" && "10px"};
+right: ${props=> props.direction === "right" && "10px"};
+top:0;
+bottom:0;
+margin:auto;
+position: absolute;
+cursor:pointer;
 
+&:hover{
+    opacity: 0.5;
+}
+
+`
+
+const Wrapper = styled.div`
+height:100%`
+const Slide = styled.div`
+display: flex;
+align-items: center;
+`
+const ImgContainer = styled.div`
+flex:1;
+height: 100%;
+`
+const InfoContainer = styled.div`
+flex:1;
+`
+
+const Image = styled.img`
+width: 168.5vh;
 `
 
 const Slider = ()=>{
     return (
         <Container>
-<Arrow>
+<Arrow direction="left">
 <IoIosArrowDropleftCircle style={{fontSize:"45"}} />
 </Arrow>
-<Arrow>
+<Wrapper>
+    <Slide>
+    <ImgContainer>
+<Image src="https://img-c.udemycdn.com/notices/web_banner/slide_2_image_udlite/e6cc1a30-2dec-4dc5-b0f2-c5b656909d5b.jpg" />
+    </ImgContainer>
+    <InfoContainer>
+
+    </InfoContainer>
+    </Slide>
+</Wrapper>
+<Arrow direction="right">
 <IoIosArrowDroprightCircle style={{fontSize:"45"}} />
 </Arrow>
         </Container>
